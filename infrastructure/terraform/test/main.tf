2397490d2c021ecdffe5f7193309e032f56b7010
Jok3r15 <gabrielmgomez15@gmail.com>
2026-06-07 15:44:51 -0600

    refactor: rename to main.tf, add provider.tf and disable backend

---
+++ infrastructure/terraform/test/main.tf
@@ -0,0 +1,38 @@
+resource "aws_s3_bucket" "b" {
+  bucket = "my-hardened-bucket"
+}
+
+resource "aws_s3_bucket_versioning" "b_versioning" {
+  bucket = aws_s3_bucket.b.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "b_public_access" {
+  bucket = aws_s3_bucket.b.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "b_encryption" {
+  bucket = aws_s3_bucket.b.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "b_lifecycle" {
+  bucket = aws_s3_bucket.b.id
+  rule {
+    id     = "log-expiry"
+    status = "Enabled"
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+  }
+}
