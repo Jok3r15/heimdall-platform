609982a503afc56c3e2c46e13a731280a2567ca1
Jok3r15 <gabrielmgomez15@gmail.com>
2026-06-07 16:14:22 -0600

    refactor: update s3 lifecycle filter to resolve provider warnings

--- infrastructure/terraform/test/main.tf
+++ infrastructure/terraform/test/main.tf
@@ -31,6 +31,11 @@ resource "aws_s3_bucket_lifecycle_configuration" "b_lifecycle" {
   rule {
     id     = "cleanup"
     status = "Enabled"
+    
+    filter {
+      prefix = ""
+    }
+
     abort_incomplete_multipart_upload {
       days_after_initiation = 7
     }
